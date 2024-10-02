b2b6e4cd9f1c89537fd4e9fbfc2914381f8cd99f
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-10-02 06:26:05 +0000

    changed views.

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
 ]
