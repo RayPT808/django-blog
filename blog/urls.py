d4e6bd2f0b3ff79026c0f64bdba59663c02eaea8
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-10-03 08:06:53 +0000

    Added url arguments

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.EventsList.as_view(), name='home'),
+    path("<int:event_id>/", views.event_detail,
+         name="event_detail") 
 ]
